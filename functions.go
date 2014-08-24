95501aaab0fbfd0683f263c1dc64fb3b6371b1ae
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-08-24 11:19:37 -0700

    Swap

--- functions.go
+++ functions.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 )
 
-func add(x int, y int) int {
+func add(x, y int) int {
 	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
 }
 
 func main() {
 	fmt.Println(add(4, 5))
+	fmt.Println(swap("first arg", "last arg"))
 }
